72f61ea96eebdcf3a4cdf44a3858ebd9e4ee5ee0
Your Name <advmenendez92@gmail.com>
2021-11-04 12:46:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import DigitalClock from "./components/DigitalClock/DigitalClock";
+import "./App.css";
+import Countdown from "./components/CountDown/CountDown";
+import Stopwatch from "./components/StopWatch/StopWatch";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <DigitalClock />
+        <Countdown />
+        <Stopwatch/>
+        
+      </div>
+    </>
   );
-}
+};
 
 export default App;
